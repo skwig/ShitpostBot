5d5022915f0524d574655b66381f2a4c2e94a235
TvojeMater <99494696+TvojeMater@users.noreply.github.com>
2022-02-15 10:27:47 +0100

    Update ChatMessageDeletedListener.cs

--- ShitpostBot/ShitpostBot.Worker/Internal/ChatMessageHandlers/ChatMessageDeletedListener.cs
+++ ShitpostBot/ShitpostBot.Worker/Internal/ChatMessageHandlers/ChatMessageDeletedListener.cs
@@ -3,8 +3,6 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
-using DSharpPlus;
-using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
 using MediatR;
 using Microsoft.Extensions.Logging;
