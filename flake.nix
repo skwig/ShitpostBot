b2682b4e7e2b45b13762d0ba30edef3eea3d8ed2
skwig <16136203+skwig@users.noreply.github.com>
2025-12-13 20:37:45 +0100

    nix dev flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        nativeDeps = with pkgs; [
+          kubernetes-helm
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = nativeDeps;
+        };
+      }
+    );
+}
