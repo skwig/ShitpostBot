3aa5b55ac33d1f32b280994c93541cc82119bc6c
skwig <16136203+skwig@users.noreply.github.com>
2025-12-14 20:41:36 +0100

    feat: migrate LinkTests from NUnit to xUnit attributes

--- src/ShitpostBot/test/ShitpostBot.Tests.Unit/LinkTests.cs
+++ src/ShitpostBot/test/ShitpostBot.Tests.Unit/LinkTests.cs
@@ -1,25 +1,25 @@
 using System;
 using FluentAssertions;
-using NUnit.Framework;
+using Xunit;
 using ShitpostBot.Domain;
 
 namespace ShitpostBot.Tests;
 
-[TestFixture]
 public class LinkTests
 {
-    [TestCase("https://streamable.com/xb6yrr", LinkProvider.Generic, "xb6yrr")]
-    [TestCase("https://www.youtube.com/watch?v=eusx0VW-m3M", LinkProvider.YouTube, "eusx0VW-m3M")]
-    [TestCase("https://www.youtube.com/watch?v=eusx0VW-m3M&t=60", LinkProvider.YouTube, "eusx0VW-m3M")]
-    [TestCase("https://youtu.be/eusx0VW-m3M", LinkProvider.YouTube, "eusx0VW-m3M")]
-    [TestCase("https://youtu.be/eusx0VW-m3M?t=60", LinkProvider.YouTube, "eusx0VW-m3M")]
-    [TestCase("https://steamcommunity.com/sharedfiles/filedetails/?id=505736710", LinkProvider.SteamWorkshop, "505736710")]
-    [TestCase("https://steamcommunity.com/sharedfiles/filedetails/?id=2067312913", LinkProvider.SteamWorkshop, "2067312913")]
-    [TestCase("https://www.idnes.cz/hry/magazin/stars-of-blood-valve-zrusena-hra.A211104_215739_bw-magazin_oma", LinkProvider.Generic, "hry/magazin/stars-of-blood-valve-zrusena-hra.A211104_215739_bw-magazin_oma")]
-    [TestCase("https://www.google.com", null, null)]
-    [TestCase("https://www.google.com/", null, null)]
-    [TestCase("https://tenor.com/view/nodding-moon-creepy-gif-14222607", null, null)]
-    [TestCase("https://media.discordapp.net/attachments/138031010951593984/905070007178911774/dontbelievehislies.gif", null, null)]
+    [Theory]
+    [InlineData("https://streamable.com/xb6yrr", LinkProvider.Generic, "xb6yrr")]
+    [InlineData("https://www.youtube.com/watch?v=eusx0VW-m3M", LinkProvider.YouTube, "eusx0VW-m3M")]
+    [InlineData("https://www.youtube.com/watch?v=eusx0VW-m3M&t=60", LinkProvider.YouTube, "eusx0VW-m3M")]
+    [InlineData("https://youtu.be/eusx0VW-m3M", LinkProvider.YouTube, "eusx0VW-m3M")]
+    [InlineData("https://youtu.be/eusx0VW-m3M?t=60", LinkProvider.YouTube, "eusx0VW-m3M")]
+    [InlineData("https://steamcommunity.com/sharedfiles/filedetails/?id=505736710", LinkProvider.SteamWorkshop, "505736710")]
+    [InlineData("https://steamcommunity.com/sharedfiles/filedetails/?id=2067312913", LinkProvider.SteamWorkshop, "2067312913")]
+    [InlineData("https://www.idnes.cz/hry/magazin/stars-of-blood-valve-zrusena-hra.A211104_215739_bw-magazin_oma", LinkProvider.Generic, "hry/magazin/stars-of-blood-valve-zrusena-hra.A211104_215739_bw-magazin_oma")]
+    [InlineData("https://www.google.com", null, null)]
+    [InlineData("https://www.google.com/", null, null)]
+    [InlineData("https://tenor.com/view/nodding-moon-creepy-gif-14222607", null, null)]
+    [InlineData("https://media.discordapp.net/attachments/138031010951593984/905070007178911774/dontbelievehislies.gif", null, null)]
     public void CreateOrDefault(string linkUri, LinkProvider? expectedLinkProvider, string? expectedLinkId)
     {
         var link = Link.CreateOrDefault(new Uri(linkUri));
