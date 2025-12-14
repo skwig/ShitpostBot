0c54f371ed5f496c73b3667f5fa7a39895f17c55
skwig <16136203+skwig@users.noreply.github.com>
2025-12-14 20:02:36 +0100

    chore: remove Newtonsoft.Json using from PostConfiguration

--- src/ShitpostBot/test/ShitpostBot.Infrastructure.Tests/ConfigTests.cs
+++ src/ShitpostBot/test/ShitpostBot.Infrastructure.Tests/ConfigTests.cs
@@ -1,6 +1,10 @@
 using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using NUnit.Framework;
 using ShitpostBot.Infrastructure;
+using ShitpostBot.Domain;
 
 namespace ShitpostBot.Infrastructure.Tests;
 
@@ -14,5 +18,15 @@ public class ConfigTests
         
         Assert.That(options, Is.Not.Null);
         Assert.That(options.PropertyNamingPolicy, Is.EqualTo(JsonNamingPolicy.CamelCase));
+    }
+
+    [Test]
+    public void PostConfiguration_ShouldConfigureCorrectly()
+    {
+        // Test that Post configuration doesn't depend on Newtonsoft.Json
+        var builder = new ModelBuilder();
+        var configuration = new PostConfiguration();
+        
+        Assert.DoesNotThrow(() => configuration.Configure(builder.Entity<Post>()));
     }
 }
