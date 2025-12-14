70df69bf7f1d1fe767a14643eff44df1296cdbe5
skwig <16136203+skwig@users.noreply.github.com>
2025-12-14 14:52:29 +0100

    refactor: extract MassTransit configuration to Infrastructure
    - Add AddShitpostBotMassTransit extension method
    - Centralizes PostgreSQL transport setup
    - Enables reuse in Worker and WebApi

--- src/ShitpostBot/src/ShitpostBot.Worker/Program.cs
+++ src/ShitpostBot/src/ShitpostBot.Worker/Program.cs
@@ -2,14 +2,11 @@ using System;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
-using CloudEventify.MassTransit;
-using MassTransit;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
-using Npgsql;
-using ShitpostBot.Infrastructure.Messages;
+using ShitpostBot.Infrastructure;
 using ShitpostBot.Worker.Features.Repost;
 using ShitpostBot.Worker.Public;
 
@@ -34,36 +31,8 @@ public class Program
             })
             .ConfigureServices((hostContext, services) =>
             {
-                var builder = new NpgsqlConnectionStringBuilder(
-                    hostContext.Configuration.GetConnectionString("ShitpostBotMessaging")
-                );
-                services.AddOptions<SqlTransportOptions>().Configure(options =>
+                services.AddShitpostBotMassTransit(hostContext.Configuration, x =>
                 {
-                    // For whatever reason this doesn't work. On insert, it throws
-                    // > `Writing values of 'Pgvector.Vector' is not supported for parameters having no NpgsqlDbType or DataTypeName. Try setting one of these values to the expected database type.`
-                    // Filling the options one by one from the builder works for some reason.
-                    // I suspect there is some weird Npgsql initialization race condition.
-                    // options.ConnectionString = hostContext.Configuration.GetConnectionString("ShitpostBotMessaging")
-                    //                            ?? throw new ArgumentNullException();
-
-                    options.Host = builder.Host;
-                    options.Port = builder.Port;
-                    options.Username = builder.Username;
-                    options.Password = builder.Password;
-                    options.Database = builder.Database;
-                });
-                services.AddPostgresMigrationHostedService();
-                services.AddMassTransit(x =>
-                {
-                    x.UsingPostgres((context, cfg) =>
-                    {
-                        cfg.ConfigureEndpoints(context);
-                        cfg.UseCloudEvents()
-                            .WithTypes(map => map
-                                .Map<ImagePostTracked>("imagePostTracked")
-                                .Map<LinkPostTracked>("linkPostTracked")
-                            );
-                    });
                     x.AddConsumer<EvaluateRepost_ImagePostTrackedHandler>();
                     x.AddConsumer<EvaluateRepost_LinkPostTrackedHandler>();
                 });
