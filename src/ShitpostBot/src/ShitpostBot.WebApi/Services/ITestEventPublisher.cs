d6561bdc43a13d55c535d32b6a36ada94720a939
skwig <16136203+skwig@users.noreply.github.com>
2025-12-15 13:16:59 +0100

    refactor: remove SSE infrastructure (ITestEventPublisher, TestEventPublisher)

--- src/ShitpostBot/src/ShitpostBot.WebApi/Services/ITestEventPublisher.cs
+++
@@ -1,9 +0,0 @@
-namespace ShitpostBot.WebApi.Services;
-
-public interface ITestEventPublisher
-{
-    Task PublishAsync<T>(string eventType, T data);
-    IAsyncEnumerable<TestEvent> SubscribeAsync(CancellationToken cancellationToken);
-}
-
-public record TestEvent(string Type, string DataJson);
