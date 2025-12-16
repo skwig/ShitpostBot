2e9171d65ba7ff0f5ae49831ce1b18b984befb31
skwig <16136203+skwig@users.noreply.github.com>
2025-12-16 17:45:15 +0100

    feat: move ConfigBotCommandHandler to Application layer

---
+++ src/ShitpostBot/src/ShitpostBot.Application/Features/BotCommands/ConfigBotCommandHandler.cs
@@ -0,0 +1,53 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using ShitpostBot.Infrastructure;
+
+namespace ShitpostBot.Application.Features.BotCommands;
+
+public class ConfigBotCommandHandler(
+    IChatClient chatClient, 
+    IOptions<RepostServiceOptions> repostServiceOptions, 
+    IHostEnvironment hostEnvironment)
+    : IBotCommandHandler
+{
+    private static readonly DateTimeOffset deployedOn;
+
+    static ConfigBotCommandHandler()
+    {
+        deployedOn = DateTimeOffset.UtcNow;
+    }
+
+    public string? GetHelpMessage() => "`about` - prints information about the bot";
+
+    public async Task<bool> TryHandle(
+        MessageIdentification commandMessageIdentification, 
+        MessageIdentification? referencedMessageIdentification,
+        BotCommand command)
+    {
+        if (command.Command != "about")
+        {
+            return false;
+        }
+
+        var messageDestination = new MessageDestination(
+            commandMessageIdentification.GuildId,
+            commandMessageIdentification.ChannelId,
+            commandMessageIdentification.MessageId
+        );
+
+        var utcNow = DateTimeOffset.UtcNow;
+        var message = $"Uptime: {Math.Round((utcNow - deployedOn).TotalHours, 2)} hours\n" +
+                      $"\n" +
+                      $"I'm also open source {chatClient.Utils.Emoji(":bugman:")} https://github.com/skwig/ShitpostBot" +
+                      $"\n" +
+                      $"Config:\n" +
+                      $"`{nameof(hostEnvironment.EnvironmentName)}: {hostEnvironment.EnvironmentName}`\n" +
+                      $"`{nameof(repostServiceOptions.Value.RepostSimilarityThreshold)}: {repostServiceOptions.Value.RepostSimilarityThreshold}`\n";
+
+        await chatClient.SendMessage(
+            messageDestination,
+            message
+        );
+        return true;
+    }
+}
