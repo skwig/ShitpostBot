0b86b82cbff0d562c43d8a6c9787ad22117e0e41
skwig <16136203+skwig@users.noreply.github.com>
2025-12-16 17:58:10 +0100

    cleanup

--- test/e2e/run-e2e-tests.sh
+++ test/e2e/run-e2e-tests.sh
@@ -27,4 +27,4 @@
 
 docker compose -f docker-compose.yml -f docker-compose.Development.Linux.yml down
 docker compose -f docker-compose.yml -f docker-compose.Development.Linux.yml up --build --wait webapi
-ijhttp test/e2e/e2e-tests.http
+ijhttp --no-progress test/e2e/e2e-tests.http
