489a91f982f198b5fef4b2d59d791a4de6fd56e2
skwig <16136203+skwig@users.noreply.github.com>
2025-12-16 11:36:50 +0100

    refactor: update ML service API interface to use new POST endpoint

--- src/ShitpostBot/src/ShitpostBot.Application/Services/IImageFeatureExtractorApi.cs
+++ src/ShitpostBot/src/ShitpostBot.Application/Services/IImageFeatureExtractorApi.cs
@@ -6,14 +6,47 @@ namespace ShitpostBot.Application.Services;
 
 public interface IImageFeatureExtractorApi
 {
-    [Get("/images/features")]
-    Task<ExtractImageFeaturesResponse> ExtractImageFeaturesAsync([AliasAs("image_url")] string imageUrl);
+    [Post("/process/image")]
+    Task<ProcessImageResponse> ProcessImageAsync([Body] ProcessImageRequest request);
 }
 
-public record ExtractImageFeaturesResponse
+public record ProcessImageRequest
 {
-    [JsonPropertyName("image_url")] public required string ImageUrl { get; init; }
-    [JsonPropertyName("image_features")] public required float[] ImageFeatures { get; init; }
+    [JsonPropertyName("image_url")] 
+    public required string ImageUrl { get; init; }
+    
+    [JsonPropertyName("embedding")] 
+    public bool Embedding { get; init; } = true;
+    
+    [JsonPropertyName("caption")] 
+    public bool Caption { get; init; } = false;
+    
+    [JsonPropertyName("ocr")] 
+    public bool Ocr { get; init; } = false;
+    
+    [JsonPropertyName("use_tesseract")] 
+    public bool UseTesseract { get; init; } = false;
+}
+
+public record ProcessImageResponse
+{
+    [JsonPropertyName("size")] 
+    public int[]? Size { get; init; }
+    
+    [JsonPropertyName("embedding")] 
+    public float[]? Embedding { get; init; }
+    
+    [JsonPropertyName("caption")] 
+    public string? Caption { get; init; }
+    
+    [JsonPropertyName("ocr")] 
+    public string? Ocr { get; init; }
+    
+    [JsonPropertyName("ocr_confidence")] 
+    public float? OcrConfidence { get; init; }
+    
+    [JsonPropertyName("ocr_engine")] 
+    public string? OcrEngine { get; init; }
 }
 
 public class ImageFeatureExtractorApiOptions
