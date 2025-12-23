bafbf41657d9495ace922498ea05abfad82439ad
skwig <16136203+skwig@users.noreply.github.com>
2025-12-23 11:28:27 +0100

    fix: assure images get tracked only once (#174)

--- src/ShitpostBot/src/ShitpostBot.Infrastructure/Migrations/20251223102043_ImageIdUnique.Designer.cs
+++ src/ShitpostBot/src/ShitpostBot.Infrastructure/Migrations/20251223102043_ImageIdUnique.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Pgvector;
@@ -12,9 +13,11 @@ using ShitpostBot.Infrastructure;
 namespace ShitpostBot.Infrastructure.Migrations
 {
     [DbContext(typeof(ShitpostBotDbContext))]
-    partial class ShitpostBotDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251223102043_ImageIdUnique")]
+    partial class ImageIdUnique
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
