f6294e0c6ffad4167a2cdbe10613cf6130921651
skwig <16136203+skwig@users.noreply.github.com>
2025-12-30 18:30:45 +0100

    Migrate PostLinkMessage to FastEndpoints

---
+++ src/ShitpostBot/src/ShitpostBot.WebApi/Endpoints/PostLinkMessageEndpoint.cs
@@ -0,0 +1,45 @@
+using FastEndpoints;
+using MediatR;
+using ShitpostBot.Application.Features.PostTracking;
+using ShitpostBot.WebApi.Services;
+
+namespace ShitpostBot.WebApi.Endpoints;
+
+public class PostLinkMessageEndpoint : Endpoint<PostLinkMessageRequest, PostMessageResponse>
+{
+    private readonly TestMessageFactory _factory;
+    private readonly IMediator _mediator;
+
+    public PostLinkMessageEndpoint(TestMessageFactory factory, IMediator mediator)
+    {
+        _factory = factory;
+        _mediator = mediator;
+    }
+
+    public override void Configure()
+    {
+        Post("/test/link-message");
+        AllowAnonymous();
+        Options(x => x.WithTags("Test"));
+    }
+
+    public override async Task HandleAsync(PostLinkMessageRequest req, CancellationToken ct)
+    {
+        var linkMessage = _factory.CreateLinkMessage(
+            req.LinkUrl,
+            req.GuildId,
+            req.ChannelId,
+            req.UserId,
+            req.MessageId,
+            req.Timestamp
+        );
+
+        await _mediator.Publish(new LinkMessageCreated(linkMessage), ct);
+
+        await SendOkAsync(new PostMessageResponse
+        {
+            MessageId = linkMessage.Identification.MessageId,
+            Tracked = true
+        }, ct);
+    }
+}
