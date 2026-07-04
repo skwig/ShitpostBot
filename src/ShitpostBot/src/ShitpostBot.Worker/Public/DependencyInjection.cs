cfbcddcd5022995c6ce250bf97b1b62ec4b5f567
skwig <16136203+skwig@users.noreply.github.com>
2026-07-04 23:28:12 +0200

    cleanup

--- src/ShitpostBot/src/ShitpostBot.Worker/Public/DependencyInjection.cs
+++ src/ShitpostBot/src/ShitpostBot.Worker/Public/DependencyInjection.cs
@@ -10,9 +10,9 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddShitpostBotWorker(this IServiceCollection serviceCollection)
     {
-        serviceCollection.AddScoped<IChatMessageCreatedListener, ChatMessageCreatedListener>();
-        serviceCollection.AddScoped<IChatMessageDeletedListener, ChatMessageDeletedListener>();
-        serviceCollection.AddScoped<IChatMessageUpdatedListener, ChatMessageUpdatedListener>();
+        serviceCollection.AddSingleton<IChatMessageCreatedListener, ChatMessageCreatedListener>();
+        serviceCollection.AddSingleton<IChatMessageDeletedListener, ChatMessageDeletedListener>();
+        serviceCollection.AddSingleton<IChatMessageUpdatedListener, ChatMessageUpdatedListener>();
 
         serviceCollection.AddHostedService<Worker>();
 
--- src/ShitpostBot/src/ShitpostBot.Worker/Worker.cs
+++ src/ShitpostBot/src/ShitpostBot.Worker/Worker.cs
@@ -5,62 +5,17 @@ namespace ShitpostBot.Worker;
 public class Worker(
     ILogger<Worker> logger,
     IChatClient chatClient,
-    IServiceScopeFactory scopeFactory) : IHostedService
+    IChatMessageCreatedListener created,
+    IChatMessageUpdatedListener updated,
+    IChatMessageDeletedListener deleted) : IHostedService
 {
     public async Task StartAsync(CancellationToken cancellationToken)
     {
         logger.LogInformation("Worker started at: {time}", DateTimeOffset.Now);
 
-        chatClient.MessageCreated += async args =>
-        {
-            await using var scope = scopeFactory.CreateAsyncScope();
-            var listeners = scope.ServiceProvider.GetServices<IChatMessageCreatedListener>();
-            foreach (var listener in listeners)
-            {
-                try
-                {
-                    await listener.HandleMessageCreatedAsync(args);
-                }
-                catch (Exception e)
-                {
-                    logger.LogError(e, "MessageCreated listener failed");
-                }
-            }
-        };
-
-        chatClient.MessageDeleted += async args =>
-        {
-            await using var scope = scopeFactory.CreateAsyncScope();
-            var listeners = scope.ServiceProvider.GetServices<IChatMessageDeletedListener>();
-            foreach (var listener in listeners)
-            {
-                try
-                {
-                    await listener.HandleMessageDeletedAsync(args);
-                }
-                catch (Exception e)
-                {
-                    logger.LogError(e, "MessageDeleted listener failed");
-                }
-            }
-        };
-
-        chatClient.MessageUpdated += async args =>
-        {
-            await using var scope = scopeFactory.CreateAsyncScope();
-            var listeners = scope.ServiceProvider.GetServices<IChatMessageUpdatedListener>();
-            foreach (var listener in listeners)
-            {
-                try
-                {
-                    await listener.HandleMessageUpdatedAsync(args);
-                }
-                catch (Exception e)
-                {
-                    logger.LogError(e, "MessageUpdated listener failed");
-                }
-            }
-        };
+        chatClient.MessageCreated += async args => await created.HandleMessageCreatedAsync(args);
+        chatClient.MessageUpdated += async args => await updated.HandleMessageUpdatedAsync(args);
+        chatClient.MessageDeleted += async args => await deleted.HandleMessageDeletedAsync(args);
 
         await chatClient.ConnectAsync();
     }
