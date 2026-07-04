7439c2ce1b4e1e2fac058e87be334921885c6e97
skwig <16136203+skwig@users.noreply.github.com>
2026-07-04 14:15:28 +0200

    feat: define IMessageFeature interface with default methods

---
+++ src/ShitpostBot/src/ShitpostBot.Application/MessageRouting/IMessageFeature.cs
@@ -0,0 +1,10 @@
+using ShitpostBot.Infrastructure;
+
+namespace ShitpostBot.Application.MessageRouting;
+
+public interface IMessageFeature
+{
+    Task<bool> TryHandleCreate(IncomingMessage created, CancellationToken ct) => Task.FromResult(false);
+    Task<bool> TryHandleUpdate(IncomingMessage old, IncomingMessage updated, CancellationToken ct) => Task.FromResult(false);
+    Task<bool> TryHandleDelete(MessageIdentification deleted, CancellationToken ct) => Task.FromResult(false);
+}
