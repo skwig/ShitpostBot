bef65b6f6d34641080506bbd33933aa2a8f05b71
skwig <16136203+skwig@users.noreply.github.com>
2026-07-04 09:06:35 +0200

    fix scoping issue

--- src/ShitpostBot/src/ShitpostBot.Worker/Public/DependencyInjection.cs
+++ src/ShitpostBot/src/ShitpostBot.Worker/Public/DependencyInjection.cs
@@ -14,9 +14,9 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddShitpostBotWorker(this IServiceCollection serviceCollection)
     {
-        serviceCollection.AddSingleton<IChatMessageCreatedListener, ChatMessageCreatedListener>();
-        serviceCollection.AddSingleton<IChatMessageDeletedListener, ChatMessageDeletedListener>();
-        serviceCollection.AddSingleton<IChatMessageUpdatedListener, ChatMessageUpdatedListener>();
+        serviceCollection.AddScoped<IChatMessageCreatedListener, ChatMessageCreatedListener>();
+        serviceCollection.AddScoped<IChatMessageDeletedListener, ChatMessageDeletedListener>();
+        serviceCollection.AddScoped<IChatMessageUpdatedListener, ChatMessageUpdatedListener>();
 
         serviceCollection.AddAllImplementationsScoped<IBotCommandHandler>(typeof(DependencyInjection).Assembly);
 
