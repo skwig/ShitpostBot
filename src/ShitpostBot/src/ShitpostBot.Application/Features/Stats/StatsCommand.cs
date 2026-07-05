b75bbefaf5813d7f7398daca3fec15a1c706d52c
skwig <16136203+skwig@users.noreply.github.com>
2026-07-05 11:31:27 +0200

    cleanup

--- src/ShitpostBot/src/ShitpostBot.Application/Features/Stats/StatsCommand.cs
+++ src/ShitpostBot/src/ShitpostBot.Application/Features/Stats/StatsCommand.cs
@@ -62,14 +62,14 @@ public class StatsCommand(IDbContext dbContext, IChatClient chatClient, IMetrics
         var newestLinkPost = linkPostStats?.NewestPostedOn ?? DateTimeOffset.MaxValue;
 
         var message =
-            $"Available ImagePosts: {availableImagePostCount} ({chatClient.Utils.RelativeTimestamp(oldestImagePost)} - {chatClient.Utils.RelativeTimestamp(newestImagePost)})\n"
-            + $"Available LinkPosts: {availableLinkPostCount} ({chatClient.Utils.RelativeTimestamp(oldestLinkPost)} - {chatClient.Utils.RelativeTimestamp(newestLinkPost)})\n"
-            + $"Total: {availableImagePostCount + availableLinkPostCount}"
+            $"Available ImagePosts: {availableImagePostCount} ({chatClient.Utils.RelativeTimestamp(oldestImagePost)} - {chatClient.Utils.RelativeTimestamp(newestImagePost)})"
+            + $"\nAvailable LinkPosts: {availableLinkPostCount} ({chatClient.Utils.RelativeTimestamp(oldestLinkPost)} - {chatClient.Utils.RelativeTimestamp(newestLinkPost)})"
+            + $"\nTotal: {availableImagePostCount + availableLinkPostCount}"
             + $"\n"
             + $"\nDeployed: {chatClient.Utils.RelativeTimestamp(metrics.DeployedOn)}"
-            + $"\nLast link save: {(metrics.LastLinkSaveTimestamp.HasValue ? chatClient.Utils.RelativeTimestamp(metrics.LastLinkSaveTimestamp.Value) : "never")}"
-            + $"\nLast image save: {(metrics.LastImageSaveTimestamp.HasValue ? chatClient.Utils.RelativeTimestamp(metrics.LastImageSaveTimestamp.Value) : "never")}"
-            + $"\nLast image evaluation: {(metrics.LastImageEvaluationTimestamp.HasValue ? chatClient.Utils.RelativeTimestamp(metrics.LastImageEvaluationTimestamp.Value) : "never")}";
+            + $"\nLast link save: {(metrics.LastLinkSaveTimestamp is not null ? chatClient.Utils.RelativeTimestamp(metrics.LastLinkSaveTimestamp.Value) : "never")}"
+            + $"\nLast image save: {(metrics.LastImageSaveTimestamp is not null ? chatClient.Utils.RelativeTimestamp(metrics.LastImageSaveTimestamp.Value) : "never")}"
+            + $"\nLast image evaluation: {(metrics.LastImageEvaluationTimestamp is not null ? chatClient.Utils.RelativeTimestamp(metrics.LastImageEvaluationTimestamp.Value) : "never")}";
 
         await chatClient.SendMessage(destination, message);
 
